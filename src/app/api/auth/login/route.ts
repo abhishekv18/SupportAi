ba89c050cc48c358995536168b0b385672f9ebc6
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2026-03-31 20:39:03 +0530

    add

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,9 +1,23 @@
+// import { scalekit } from "@/lib/scalekit";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(req: NextRequest) {
+//     const redirectUri=`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+//   const authorizationUrl = scalekit.getAuthorizationUrl(redirectUri);
+//   console.log("Redirecting to:", authorizationUrl);
+//     return NextResponse.redirect(authorizationUrl, 302);
+// }
 import { scalekit } from "@/lib/scalekit";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const redirectUri=`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+  const redirectUri = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
+
+  console.log("Redirect URI:", redirectUri); // DEBUG
+
   const authorizationUrl = scalekit.getAuthorizationUrl(redirectUri);
+
   console.log("Redirecting to:", authorizationUrl);
-    return NextResponse.redirect(authorizationUrl, 302);
+
+  return NextResponse.redirect(authorizationUrl, 302);
 }
